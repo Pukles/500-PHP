f95a7b759ed0d2fb64ca53704a448b493e0537c4
Pukles <arthurpuk@terra.com.br>
2018-01-20 17:01:14 -0200

    add func2

---
+++ aula02/ex-funcao.php
@@ -0,0 +1,12 @@
+// funcao
+
+funcao que receba um numero e retorne se ele é par ou ímpar;
+
+
+funcao que receba 2 numeros e retorne o maior;
+
+
+funcao que receba um array[nome, idade], retornar se a pessoa é maior de idade;
+
+
+funcao que receba um array [nome, idade, temHabilitacao(boolean)], retorna se a pessoa pode dirigir
---
+++ aula02/func2.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<pre>";
+
+function soma ($a = 0 , $b = 1) {
+	return $a + $b;
+	}
+
+
+echo soma(2,5);
+
+echo "<hr>";
+
